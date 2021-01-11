558705a4071c6d35d3ab770d2cb448aa11c622df
MnemosParasynth <jorgegarcia_tx@outlook.com>
2021-01-11 19:22:17 +0000

    Began to modify for better story flow.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-import { storyline } from './storyline.js';
+import { storyline } from './storyline.js'; // Imports storyline
+//const storyline = new Storyline // Idk what I'm doing
 
-const parent = document.getElementById('story');
-const button = document.createElement('button');
+const parent = document.getElementById('story'); 
+const button = document.createElement('button'); // Create button
 
+// Asks for char name
 document.getElementById("start").addEventListener('click', () => {
   let name = prompt("What will be your character's name?");
   document.getElementById('charName').innerHTML = `Your character's name is ${name}.`;
@@ -13,35 +15,41 @@ document.getElementById("start").addEventListener('click', () => {
   story(name);
 });
 
-// Function that creates a button and appends it
+// Function that creates a button and appends to the body
 const createButton = () => {
   button.innerHTML = "Continue";
-  console.log('Button inner html created');
   parent.appendChild(button);
-  console.log('Child appended');
+  console.log('Button created /');
 }
 
+// Write story
+
+
+// Creates story
 const story = charName => {
-  button.innerHTML = "Continue";
-  parent.appendChild(button);
+  createButton(); //Initial button
   button.addEventListener('click', () => {
+    console.log(`Character: ${charName} /`);
+    document.getElementById('story').innerHTML = `
+      Your name is ${charName} and you go to Akusawa High School. ${storyline.part1} <br>
+    `; //Intro
 
-    console.log(`Character: ${charName}`);
-    document.getElementById('story').innerHTML = `Your name is ${charName} and you go to Akusawa High School. ${storyline.part1}`;
-
-    createButton();
-    console.log("Button created");
+    //button.removeEventListener('click');
+    createButton(); //Creates button for next
     button.addEventListener('click', () => {
-      document.getElementById('story').innerHTML = storyline.part2;
+      document.getElementById('story').innerHTML = `${storyline.part2} <br>`;
+      createButton();
+      /*button.addEventListener('click', () => {
+        document.getElementById('story').innerHTML = `${storyline.dia} <br>`;
+      })*/
     })
 
   });
 };
 
-//const options = () => { }
-
-const restart = () => {
+document.getElementById("restart").addEventListener('click', () => {
   document.getElementById('charName').innerHTML = " ";
   document.getElementById('story').innerHTML = " ";
   console.log('Cleared story');
-};
+  console.clear();
+});
--- storyline.js
+++ storyline.js
@@ -1,5 +1,13 @@
-//Array or storyline
+//Storyline
+/*function Storyline(charName) {
+  part1: "You are part of the SoulWorker gaming club, along with three friends who are girls, two are your seniors and one is your junior. It is after school now and so you go to the club room for club activities. \n ";
+  part2: "You open the door and inside, and you find Itsuki, Shiro, and Arua. All of them are on their computers, playing SoulWorker. All of them look up to you. \n ";
+  dia: `${charName}, I love you \n `;
+}
+
+export { storyline };*/
+
 export const storyline = {
   part1: "You are part of the SoulWorker gaming club, along with three friends who are girls, two are your seniors and one is your junior. It is after school now and so you go to the club room for club activities. \n ",
-  part2: "You open the door and inside, you find Itsuki, Shiro, and Arua. All of them are on their computers, playing SoulWorker. All of them look up to you. \n"
-};
+  part2: "You open the door and inside, and you find Itsuki, Shiro, and Arua. All of them are on their computers, playing SoulWorker. All of them look up to you. \n "
+}
